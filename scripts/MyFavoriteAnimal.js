31c51b5e6774638653ea529b9bc8c72f3afa8983
salhernandez <salvhernandez@csumb.edu>
2017-02-15 18:23:26 +0000

    clean up

--- scripts/MyFavoriteAnimal.js
+++
@@ -1,9 +0,0 @@
-// scripts/MyFavoriteAnimal.js
-import * as React from 'react';
-export class MyFavoriteAnimal
-
-extends React.Component {
- render() {
- return <li>{this.props.animal}</li>;
- }
-}
--- scripts/Socket.js
+++
@@ -1,8 +0,0 @@
-import * as SocketIO from 'socket.io-client';
-export var Socket = SocketIO.connect();
-export var Disconnected = Socket.disconnect();
-
-export function broadcastToAll(message){
-    Socket.emit('coding event', message);
-    console.log(message);
-}
