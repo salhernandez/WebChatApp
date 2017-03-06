922fd4947ce446d30e81eae3769aee711ca267c4
Salvador Hernandez <salvhernandez@csumb.edu>
2017-03-06 20:44:32 +0000

    reverted to original tests

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -10,19 +10,14 @@ class ServerIntegrationTestCase(
         return app.app
     
     #checks that the page is live
-    # def test_page_response(self):
-    #     response = urllib2.urlopen(self.get_server_url())
-    #     self.assertEqual(response.code, 200)
+    def test_page_response(self):
+        response = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
     
     #checks that the page returns something
     def test_server_sends_hello(self):
         r = requests.get(self.get_server_url())
         self.assertGreater(len(r.text), 0)
-    
-    #makes sure that it is an html page
-    def test_getty_images(self):
-        r = requests.get(self.get_server_url())
-        self.assertIn(r.text[0], "<html>")
     
 if __name__ == '__main__':
     unittest.main()
