1d86e4e9ef5f7b92143818692d8f3ac2581886a2
Salvador Hernandez <salvhernandez@csumb.edu>
2017-03-06 17:18:11 +0000

    leap of faith 2

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -8,11 +8,16 @@ class ServerIntegrationTestCase(
 ):
     def create_app(self):
         return app.app
-        
+    
+    #checks that the page is live
     def test_page_response(self):
         response = urllib2.urlopen(self.get_server_url())
         self.assertEqual(response.code, 200)
-
+    
+    #checks that the page returns something
+    def test_server_sends_hello(self):
+        r = requests.get(self.get_server_url())
+        self.assertGreater(len(r.text), 0)
     
 if __name__ == '__main__':
     unittest.main()
