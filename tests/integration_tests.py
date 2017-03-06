f04e37cf0324adab09b0b58f810d5fc5cb9821b4
Salvador Hernandez <salvhernandez@csumb.edu>
2017-03-06 09:21:30 +0000

    leap of faith

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,20 +1,15 @@
 import app, unittest, flask_testing, requests
-import flask
 import urllib2
+
+from flask_testing import LiveServerTestCase
 
 class ServerIntegrationTestCase(
     flask_testing.LiveServerTestCase
 ):
     def create_app(self):
-        app = flask.Flask(__name__)
-        app.config['TESTING'] = True
-        # Default port is 5000
-        app.config['LIVESERVER_PORT'] = 8080
-        # Default timeout is 5 seconds
-        app.config['LIVESERVER_TIMEOUT'] = 10
-        return app
-
-    def test_server_is_up_and_running(self):
+        return app.app
+        
+    def test_page_response(self):
         response = urllib2.urlopen(self.get_server_url())
         self.assertEqual(response.code, 200)
 
